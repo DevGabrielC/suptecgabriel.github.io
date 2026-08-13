a07b6bf367f9bae28564be2e904ca2071c3bdd1f
T0540229 <T0540229@fcacco.br>
2026-08-13 01:52:26 -0300

    :bug: fix: Adiciona vite.config.ts para corrigir base path do pages

---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Avisa o Vite que o site ficará hospedado dentro desta subpasta no GitHub Pages
+  base: '/suptecgabriel.github.io/',
+});
